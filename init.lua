0ff02adba71d59957291b2e039daab148f24a83b
keis8221 <kei_s8221@icloud.com>
2023-12-08 22:33:34 +0900

    add barbar.nvim

--- init.lua
+++ init.lua
@@ -69,6 +69,18 @@ vim.opt.rtp:prepend(lazypath)
 require('lazy').setup({
   -- NOTE: First, some plugins that don't require any configuration
 
+  -- barbar.nvim
+  {
+    'romgrk/barbar.nvim',
+    dependencies = {
+      'lewis6991/gitsigns.nvim', -- OPTIONAL: for git status
+      'nvim-tree/nvim-web-devicons', -- OPTIONAL: for file icons
+    },
+    init = function() vim.g.barbar_auto_setup = false end,
+    opts = {},
+    version = '^1.0.0', -- optional: only update when a new 1.x version is released
+  },
+
   -- neovim transparent
   {
     "xiyaowong/nvim-transparent",
